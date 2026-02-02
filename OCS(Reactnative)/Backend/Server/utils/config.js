728f7968e21215298dcb8efa862e954fe71b8169
Pranjal Chirmade <98344714+pranjal0511@users.noreply.github.com>
2026-02-03 00:22:50 +0530

    UPdated

--- OCS(Reactnative)/Backend/Server/utils/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  SECRET: 'car_service_secret',
-  SALT_ROUND: 10
-}
--- OCS(Reactnative)/Backend/Server/utils/result.js
+++
@@ -1,5 +0,0 @@
-exports.createResult = (error, data) => {
-  if (error)
-    return { status: 'error', error }
-  return { status: 'success', data }
-}
